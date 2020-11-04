6686c5deead918155a33c4167c0829b92cd0ff03
Marcio Rodrigues <luiz.rodrigues@eduzz.com>
2020-11-03 21:32:43 -0300

    fix: apagando arquivos default

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home Page</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
